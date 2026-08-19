25ee474fb85c50e2e250fca3c0fb470df9b8cb45
Carlos O'Ryan <coryan@google.com>
2026-08-19 22:21:47 +0000

    chore: update librarian circa 2026-08-18 (#470)
    In this version of librarian we can automatically import some packages
    with a `@_spi()` attribute. That is handy to hide implementation details
    within `GoogleCloudWkt` and `GoogleCloudGax`.

--- Sources/GoogleCloudOSLoginCommon/PosixAccount.swift
+++ Sources/GoogleCloudOSLoginCommon/PosixAccount.swift
@@ -15,7 +15,7 @@
 // limitations under the License.
 
 import Foundation
-import GoogleCloudWkt
+@_spi(GoogleCloudInternal) import GoogleCloudWkt
 
 /// The POSIX account information associated with a Google account.
 public struct PosixAccount: Codable, Equatable, GoogleCloudWkt._AnyPackable,
--- Sources/GoogleCloudOSLoginCommon/SshPublicKey.swift
+++ Sources/GoogleCloudOSLoginCommon/SshPublicKey.swift
@@ -15,7 +15,7 @@
 // limitations under the License.
 
 import Foundation
-import GoogleCloudWkt
+@_spi(GoogleCloudInternal) import GoogleCloudWkt
 
 /// The SSH public key information associated with a Google account.
 public struct SshPublicKey: Codable, Equatable, GoogleCloudWkt._AnyPackable,
