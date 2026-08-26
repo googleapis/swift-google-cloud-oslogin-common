a4c47099ea759dd7b6b3a1b549cf0d1274d04a34
Carlos O'Ryan <coryan@google.com>
2026-08-26 18:08:02 +0000

    cleanup: rename `GoogleCloudWkt` (#543)
    Rename `GoogleCloudWkt` to `GoogleCloudWKT` because the convention in
    Swift is to use all caps for acronyms, in this case `WKT` == Well-Known
    Types.

--- Package.swift
+++ Package.swift
@@ -30,7 +30,7 @@ let package = Package(
     .target(
       name: "GoogleCloudOSLoginCommon",
       dependencies: [
-        .product(name: "GoogleCloudWkt", package: "wkt")
+        .product(name: "GoogleCloudWKT", package: "wkt")
       ],
     )
   ]
--- Sources/GoogleCloudOSLoginCommon/PosixAccount.swift
+++ Sources/GoogleCloudOSLoginCommon/PosixAccount.swift
@@ -15,10 +15,10 @@
 // limitations under the License.
 
 import Foundation
-@_spi(GoogleCloudInternal) import GoogleCloudWkt
+@_spi(GoogleCloudInternal) import GoogleCloudWKT
 
 /// The POSIX account information associated with a Google account.
-public struct PosixAccount: Codable, Equatable, GoogleCloudWkt._AnyPackable,
+public struct PosixAccount: Codable, Equatable, GoogleCloudWKT._AnyPackable,
   Sendable
 {
   /// Only one POSIX account can be marked as primary.
@@ -74,10 +74,10 @@ public struct PosixAccount: Codable, Equatable, GoogleCloudWkt._AnyPackable,
   public static var _anyTypeUrl: Swift.String {
     return "type.googleapis.com/google.cloud.oslogin.common.PosixAccount"
   }
-  public init(fromAny any: GoogleCloudWkt.`Any`) throws {
-    self = try GoogleCloudWkt._slowAnyDeserialize(Self.self, from: any)
+  public init(fromAny any: GoogleCloudWKT.`Any`) throws {
+    self = try GoogleCloudWKT._slowAnyDeserialize(Self.self, from: any)
   }
-  public func _pack() throws -> GoogleCloudWkt.Struct {
-    return try GoogleCloudWkt._slowAnySerialize(message: self)
+  public func _pack() throws -> GoogleCloudWKT.Struct {
+    return try GoogleCloudWKT._slowAnySerialize(message: self)
   }
 }
