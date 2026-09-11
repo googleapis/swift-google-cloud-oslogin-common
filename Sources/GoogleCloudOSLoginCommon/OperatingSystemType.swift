561d28dcd4e31f75f6ef89f7f86396619412bcd3
Carlos O'Ryan <coryan@google.com>
2026-09-11 20:11:51 +0000

    feat: update librarian with serialize-enums-as-strings (#829)
    Update librarian to use strings when serializing enums. Fixed a number
    of unit / integration tests.

--- Sources/GoogleCloudOSLoginCommon/OperatingSystemType.swift
+++ Sources/GoogleCloudOSLoginCommon/OperatingSystemType.swift
@@ -113,9 +113,9 @@ public enum OperatingSystemType: Codable, Equatable, Sendable {
   public func encode(to encoder: Encoder) throws {
     var container = encoder.singleValueContainer()
     switch self {
-    case .unspecified: return try container.encode(0)
-    case .linux: return try container.encode(1)
-    case .windows: return try container.encode(2)
+    case .unspecified: return try container.encode("OPERATING_SYSTEM_TYPE_UNSPECIFIED")
+    case .linux: return try container.encode("LINUX")
+    case .windows: return try container.encode("WINDOWS")
     case .unknownIntValue(let v): return try container.encode(v)
     case .unknownStringValue(let v): return try container.encode(v)
     }
